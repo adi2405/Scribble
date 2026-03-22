dd93b66540c9c89cd5d1a48aee374dd93540be05
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-03-22 20:37:29 +0530

    03: editor tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button>Click me</Button>
+      <Link href={"/documents/123"}>
+        <Button>Click me</Button>
+      </Link>
     </div>
   );
 }
