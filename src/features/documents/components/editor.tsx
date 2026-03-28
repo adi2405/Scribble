f2eb2579760d4d4472a42381ecedd9ac9ba8ef3a
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-03-28 11:38:57 +0530

    05: margin ruler

--- src/features/documents/components/editor.tsx
+++ src/features/documents/components/editor.tsx
@@ -13,6 +13,7 @@ import { TaskItem, TaskList } from "@tiptap/extension-list";
 import { LineHeightExtension } from "@/extensions/line-height";
 import { TextStyle, FontFamily, FontSize } from "@tiptap/extension-text-style";
 
+import { Ruler } from "./ruler";
 import { useEditorStore } from "@/store/use-editor-store";
 
 export function Editor() {
@@ -80,6 +81,7 @@ export function Editor() {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
