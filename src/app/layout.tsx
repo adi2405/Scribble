491ede5e5800513b4f83a3f575688593997789a9
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-03-31 13:28:04 +0530

    08: template gallery component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Inter, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
@@ -32,7 +33,9 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
+import { HomeView } from "@/features/home/views/home-view";
 
 export default function Home() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <Link href={"/documents/123"}>
-        <Button>Click me</Button>
-      </Link>
-    </div>
-  );
+  return <HomeView />;
 }
