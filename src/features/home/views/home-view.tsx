a158461f4cd7c3d77ff2a23f9238ed38c9ed6018
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-03-31 14:21:11 +0530

    09: convex setup

--- src/features/home/views/home-view.tsx
+++ src/features/home/views/home-view.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useQuery } from "convex/react";
+
 import { Navbar } from "../components/navbar";
+import { api } from "../../../../convex/_generated/api";
 import { TemplatesGallery } from "../components/templates-gallery";
 
 export function HomeView() {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 p-4 bg-white">
@@ -9,6 +20,9 @@ export function HomeView() {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
