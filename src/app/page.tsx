979db3d5f38c9090823bb0b487466b57d6890826
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-04-06 00:40:21 +0530

    21: fixed build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
+
 import { HomeView } from "@/features/home/views/home-view";
+import { FullscreenLoader } from "@/components/custom/fullscreen-loader";
 
 export default function Home() {
-  return <HomeView />;
+  return (
+    <Suspense fallback={<FullscreenLoader label="Loading..." />}>
+      <HomeView />
+    </Suspense>
+  );
 }
--- src/features/documents/server/actions.ts
+++ src/features/documents/server/actions.ts
@@ -3,6 +3,7 @@
 import { ConvexHttpClient } from "convex/browser";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 
+import { hslToHex } from "@/lib/utils";
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
 
@@ -10,6 +11,16 @@ const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export async function getDocuments(ids: Id<"documents">[]) {
   return await convex.query(api.documents.getByIds, { ids });
+}
+
+function getUserColor(name: string) {
+  const nameToNumber = name
+    .split("")
+    .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+
+  const hue = Math.abs(nameToNumber) % 360;
+
+  return hslToHex(hue, 80, 60);
 }
 
 export async function getUsers() {
@@ -25,12 +36,17 @@ export async function getUsers() {
     organizationId: [orgId],
   });
 
-  const users = response.data.map((user) => ({
-    id: user.id,
-    name:
-      user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
-    avatar: user.imageUrl,
-  }));
+  const users = response.data.map((user) => {
+    const name =
+      user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous";
+
+    return {
+      id: user.id,
+      name,
+      avatar: user.imageUrl,
+      color: getUserColor(name),
+    };
+  });
 
   return users;
 }
